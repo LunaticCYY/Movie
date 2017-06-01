9a3bcb109683930e2adf2c47a1646ced86b94b6a
陈圆圆 <2972512952@qq.com>
2017-06-01 18:39:50 +0800

    Movie
    2017/6/1-2017/6/21 Movie

---
+++ Movie/Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Movie.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Cid { get; set; }
+        [Required]
+        public int Uid { get; set; }
+        [Required]
+        public int Vid { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required]
+        public string CommentTime { get; set; }
+    }
+}
---
+++ Movie/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Movie.Models
+{
+    public class User
+    {
+        [Key]
+        public int Uid { get; set; }
+        [Required]
+        [StringLength(16,MinimumLength = 3)]
+        public string NickName { get; set; }
+        [Required]
+        [StringLength(16, MinimumLength = 6)]
+        public string Password { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$")]
+        public string Email { get; set; }
+        public int Privilege { get; set; }
+    }
+    public class OracleDbContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("SCOTT");
+        }
+        public DbSet<User> User { get; set; }
+        //public DbSet<Video> Video { get; set; }
+        //public DbSet<History> History { get; set; }
+        //public DbSet<Comment> Comment { get; set; }
+    }
+}
