c70a308ee09e52b5865206798c9c693d09c09bc5
陈圆圆 <2972512952@qq.com>
2017-06-05 15:30:42 +0800

    2017/6/5
    增加点击视频触发增加历史记录。

--- Movie/Controllers/UserOperationController.cs
+++ Movie/Controllers/UserOperationController.cs
@@ -24,15 +24,23 @@ namespace Movie.Controllers
 
         public ActionResult Detail(int? id)
         {
-            //if (id == null)
-            //{
-            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            //}
             var vi = db.Videos.Find(id);
-            //if (vi == null)
-            //{
-            //    return HttpNotFound();
-            //}
+            if (Request.Cookies["uid"] != null)
+            {
+                HttpCookie hc = Request.Cookies["uid"];
+                int uid = int.Parse(hc.Value);
+                History history = new History();
+                var MaxId = db.Histories.Any() ? db.Histories.Max(p => p.HistoryId) : 0;
+                history.HistoryId = MaxId + 1;
+                history.UserId = uid;
+                history.Vid = vi.VideoId;
+                history.HistoryTime = DateTime.Now.ToString();
+                db.Histories.Add(history);
+                if (db.SaveChanges() != 1)
+                {
+                    ModelState.AddModelError("", "服务器错误");
+                }
+            }
             return View(vi);
         }
 
