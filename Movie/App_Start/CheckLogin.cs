0cb80b4a49271aec7e06f7cb352db8b4bd119188
tyy <tyy_man@outlook.com>
2017-06-13 17:01:41 +0800

    登入验证

---
+++ Movie/App_Start/CheckLogin.cs
@@ -0,0 +1,22 @@
+﻿using Movie.Models;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Movie.App_Start
+{
+    public class CheckLogin : ActionFilterAttribute
+    {
+        private MovieContext db = new MovieContext();
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+          if(HttpContext.Current.Request.Cookies["uid"] == null)
+            {
+                filterContext.Result = new RedirectResult("/Account/Login");
+            }
+        }
+
+    }
+}
