2cd80a50835a93d9b1a43d7e022da9604c2b57d8
陈圆圆 <2972512952@qq.com>
2017-06-15 18:10:14 +0800

    2017/6/15 还差分页、异步刷新、观看记录跳转

---
+++ Movie/Models/Total.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie.Models
+{
+    public class Total
+    {
+        public int UserId { get; set; }
+        public string NickName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Privilege { get; set; }
+        public int VideoId { get; set; }
+        public string Vname { get; set; }
+        public string Vurl { get; set; }
+        public string Thumbnail { get; set; }
+        public int ViewedNum { get; set; }
+        public string UploadTime { get; set; }
+        public string Vtype { get; set; }
+        public string Vinfo { get; set; }
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public string CommentTime { get; set; }
+        public int HistoryId { get; set; }
+        public string HistoryTime { get; set; }
+        public int FavoriteId { get; set; }
+        public string FavoriteTime { get; set; }
+    }
+}
